61ce66bfb448d21be2d21e111cc9b5d1a9bdf456
Jacob Jobse <jakejobse1@gmail.com>
2025-02-11 10:13:40 -0700

    Final V
    11-02-2025

--- src/exercise2/Driver.java
+++ src/exercise2/Driver.java
@@ -1,77 +1,54 @@
-//Jacobs version 20:38 10-02-2025
-
+// Jacobs version 10:02 11-02-2025
 package exercise2;
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class Driver
-{
+public class Driver {
+	
+    public static final int SIZE = 100;
+    public static final int UPPER_BOUND = 10;
+    public static void main(String[] args) {
 
-	public static final int SIZE = 100;
-	public static final int UPPER_BOUND = 10;
+        Integer[] nums = new Integer[SIZE];
+        Random rand = new Random();
+        int randnum = rand.nextInt(UPPER_BOUND);
+        nums[0] = randnum;
+        System.out.println(nums[0]);
 
-	public static void main( String[] args )
-	{
-
-		Integer[] nums = new Integer[SIZE];
-		Random rand = new Random();
-		int randnum = rand.nextInt( UPPER_BOUND );
-		nums[0] = randnum;
-
-		for( int i = 1; i < SIZE; i++ )
-		{
-			randnum = rand.nextInt( UPPER_BOUND );
-			nums[i] = nums[i - 1] + randnum;
-			System.out.println( nums[i] );
-		}
-		  // Display the sorted list
-        System.out.println("Sorted List:");
-        for (int num : nums) {
-            System.out.print(num + " ");
+        for (int i = 1; i < SIZE; i++) {
+            randnum = rand.nextInt(UPPER_BOUND);
+            nums[i] = nums[i - 1] + randnum;
+            System.out.println(nums[i]);
         }
-        System.out.println("\n");
 
-        // Get user input
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer to search: ");
-        int target = scanner.nextInt();
-
-        // Perform binary search
-        int index = binarySearch(nums, target);
-
-        // Display search result
-        if (index != -1) {
-            System.out.println("Target " + target + " found at index " + index + ".");
+        int targetNum = scanner.nextInt();
+        int index = binarySearch(nums, targetNum);
+        
+        if (index == -1) {
+            System.out.println("-1 Not Found");
         } else {
-            System.out.println("Target " + target + " not found in the list.");
+            System.out.println("Target " + targetNum + " found at index " + index + ".");
         }
-
         scanner.close();
     }
+    public static int binarySearch(Integer[] sortedArray, int targetNum) {
+        int startIndex = 0;
+        int endIndex = sortedArray.length - 1;
 
-    /**
-     * Binary Search Algorithm (Manually Implemented)
-     * @param arr Sorted array of Integers
-     * @param target Value to find
-     * @return Index of target if found, otherwise -1
-     */
-    public static int binarySearch(Integer[] arr, int target) {
-        int left = 0, right = arr.length - 1;
+        while (startIndex <= endIndex) {
+            int middleIndex = (startIndex + endIndex) / 2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2; // Prevents integer overflow
-
-            if (arr[mid].equals(target)) {
-                return mid; // Target found
-            } else if (arr[mid] < target) {
-                left = mid + 1; // Search right half
+            if (sortedArray[middleIndex].equals(targetNum)) {
+                return middleIndex;
+            } else if (sortedArray[middleIndex] < targetNum) {
+                startIndex = middleIndex + 1;
             } else {
-                right = mid - 1; // Search left half
+                endIndex = middleIndex - 1;
             }
         }
-
-        return -1; // Target not found
+        return -1;
     }
-
 }
